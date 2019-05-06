949e9f9de04ed3dfa11d9d20d78fbddd73b85e81
Marin Godechot <marin.godechot@gmail.com>
2019-05-06 16:29:11 +0200

    Initial commit

---
+++ project/FrontendCommands.scala
@@ -0,0 +1,10 @@
+// Source: https://blog.usejournal.com/react-with-play-framework-2-6-x-a6e15c0b7bd
+/**
+  * Frontend build commands.
+  */
+object FrontendCommands {
+  val dependencyInstall: String = "npm install"
+  val test: String = "npm run test"
+  val serve: String = "npm run start"
+  val build: String = "npm run build"
+}
