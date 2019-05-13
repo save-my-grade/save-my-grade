475a1e8f80fecc10b738bf3c2aa5f3ddd0aadc2b
Marin Godechot <marin.godechot@gmail.com>
2019-05-13 15:13:10 +0200

    navbar on home page

--- ui/src/pages/HomePage.js
+++ ui/src/pages/HomePage.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from "../images/logo_text.png"
+import classNames from "classnames";
 
 function HomePage() {
+    const [isActive, setActive] = useState(false);
+
+    function toggleMenu() {
+        isActive ? setActive(false) : setActive(true);
+    }
+
+    useEffect(() => {
+        document.body.classList.add('has-navbar-fixed-top');
+        return function cleanup() {
+            document.body.classList.remove('has-navbar-fixed-top');
+        }
+    });
     return (
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="/home/">
-                    <img src={logo} alt="logo"/>
-                </a>
+        <div>
+            <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation"
+                 style={{backgroundColor: "#fafafa"}}>
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="/home/" style={{paddingLeft: "5%"}}>
+                        <img src={logo} alt="logo"/>
+                    </a>
+
+                    <a role="button" className={classNames("navbar-burger burger", {"is-active": isActive})}
+                       aria-label="menu" aria-expanded="false"
+                       data-target="navbarMenu" onClick={toggleMenu}>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+                <div id="navbarMenu" className={classNames("navbar-menu", {"is-active": isActive})}>
+                    <div className="navbar-end">
+                        <a className="navbar-item">
+                            Ajouter une fiche
+                        </a>
+                    </div>
+                </div>
+            </nav>
+            <div className="page-content" style={{paddingTop: 40}}>
+                <h1 className="title">Matières</h1>
             </div>
-        </nav>
+        </div>
     );
 
 }
