826d28bd03236c1ec2af8a56362e985bfe4ed3c9
Marin Godechot <marin.godechot@gmail.com>
2019-05-19 20:14:40 +0200

    add formik

--- ui/src/pages/LoginPage.js
+++ ui/src/pages/LoginPage.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo_text from "../images/logo_text.png";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 
 function LoginPage({handleLogin}) {
+    const [signUpFormVisibility, setSignUpFormVisibility] = useState(false);
     return (
         <div className="page-container" id="login-page-container">
             <div className="content-wrap">
@@ -20,6 +22,13 @@ function LoginPage({handleLogin}) {
                                 <button className="button is-link is-large"
                                         onClick={handleLogin}>Connexion
                                 </button>
+                                <br/>
+                                <button className="button is-link is-large is-outlined"
+                                        style={{marginTop: 10}}
+                                        onClick={() => setSignUpFormVisibility(true)}>
+                                    Inscription
+                                </button>
+                                {signUpFormVisibility && <SignupForm/>}
                             </div>
                         </div>
                     </div>
@@ -27,6 +36,40 @@ function LoginPage({handleLogin}) {
             </div>
         </div>
     );
+}
+
+function SignupForm() {
+    return (
+        <Formik
+            initialValues={{email: '', password: ''}}
+            validate={values => {
+                let errors = {};
+                if (!values.email) {
+                    errors.email = 'Required';
+                }
+                return errors;
+            }}
+            onSubmit={(values, {setSubmitting}) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            {({isSubmitting}) => (
+                <Form>
+                    <Field type="email" name="email"/>
+                    <ErrorMessage name="email" component="div"/>
+                    <Field type="password" name="password"/>
+                    <ErrorMessage name="password" component="div"/>
+                    <button type="submit" disabled={isSubmitting}>
+                        S'inscrire
+                    </button>
+                </Form>
+            )}
+        </Formik>
+    );
+
 }
 
 export default LoginPage;
