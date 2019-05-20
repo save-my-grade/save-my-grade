193e02d53cea006f8923c308277051f7e4e7ecfe
Marin Godechot <marin.godechot@gmail.com>
2019-05-20 10:58:06 +0200

    fix sql conf

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,16 +2,16 @@
 
 -- !Ups
 
-CREATE TABLE User
+CREATE TABLE user
 (
-    id         bigint(20)   NOT NULL AUTO_INCREMENT,
-    email      varchar(255) NOT NULL UNIQUE,
-    password   varchar(255) NOT NULL,
-    token      varchar(255),
-    first_name varchar(255),
-    last_name  varchar(255),
-    is_admin   boolean      NOT NULL,
-    PRIMARY KEY (id)
+  id         bigint(20)   NOT NULL AUTO_INCREMENT,
+  email      varchar(255) NOT NULL UNIQUE,
+  password   varchar(255) NOT NULL,
+  token      varchar(255),
+  first_name varchar(255),
+  last_name  varchar(255),
+  is_admin   boolean      NOT NULL,
+  PRIMARY KEY (id)
 );
 
 -- !Downs
