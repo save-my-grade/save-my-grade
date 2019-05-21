7f040141b6fa3aaff84f35646e610de05f0b5112
Marin Godechot <marin.godechot@gmail.com>
2019-05-21 09:46:32 +0200

    remove UserStore

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,15 +1,11 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import models.User;
-import models.UserStore;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
-
-import java.util.Set;
 
 public class UserController extends Controller {
     public Result create(Http.Request request) {
@@ -22,13 +18,5 @@ public class UserController extends Controller {
         user.save();
         JsonNode jsonObject = Json.toJson(user);
         return created(Util.createResponse(jsonObject, true));
-    }
-
-    public Result listUsers() {
-        Set<User> result = UserStore.getInstance().getAllUsers();
-        ObjectMapper mapper = new ObjectMapper();
-
-        JsonNode jsonData = mapper.convertValue(result, JsonNode.class);
-        return ok(Util.createResponse(jsonData, true));
     }
 }
