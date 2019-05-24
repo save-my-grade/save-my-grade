9b27e99f87f340030898a9c8fc6a56f27f5b6c4b
Marin Godechot <marin.godechot@gmail.com>
2019-05-24 12:59:56 +0200

    sheet creation form

--- ui/src/components/forms/CreateSheetForm.js
+++ ui/src/components/forms/CreateSheetForm.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {ErrorMessage, Field, Form, Formik} from "formik";
 import classNames from "classnames";
@@ -10,10 +10,46 @@ CreateSheetForm.propTypes = {
     connectedUser: PropTypes.object.isRequired
 };
 
+function CourseOptions({courses, selectedCycle}) {
+    return (
+        <React.Fragment>
+            {courses.map((course) =>
+                course.cycle === selectedCycle && <option value={course.id} key={course.id}>{course.name}</option>)}
+        </React.Fragment>
+    );
+
+}
+
 function CreateSheetForm({connectedUser, successCallback, failureCallback}) {
+    const [selectedCycle, setSelectedCycle] = useState("");
+
+    const [areCoursesLoading, setAreCoursesLoading] = useState(true);
+    const [courses, setCourses] = useState({});
+    useEffect(() => {
+        setAreCoursesLoading(true);
+        axios({
+            method: 'get',
+            url: '/api/courses',
+        })
+            .then((response) => {
+                let fetchedCourses = response.data.body;
+                setCourses(fetchedCourses);
+                setAreCoursesLoading(false)
+            }).catch((e) => {
+            alert(e);
+        })
+    }, []);
+
+
     return (
         <Formik
-            initialValues={{authorId: connectedUser.id, courseId: '', name: '', tags: ''}}
+            initialValues={{
+                authorId: connectedUser.id,
+                courseId: '',
+                name: '',
+                tags: '',
+                filePath: '/change/this/' + Math.random()
+            }}
             validate={values => {
                 let errors = {};
                 if (!values.courseId) {
@@ -47,14 +83,29 @@ function CreateSheetForm({connectedUser, successCallback, failureCallback}) {
                     <Field type="text" name="name" className="input" placeholder="Nom de la fiche"/>
                     <ErrorMessage name="name" component="div"/>
                     <div style={{marginTop: 10}}/>
-                    <div className="select">
-                        <Field component="select" name="courseId" placeholder="Matière">
-                            <option value={"maths"}>Mathématiques</option>
-                            <option value={"physique"}>Physique</option>
-                            <option value={"java"}>Java</option>
+                    <div className={classNames("select", {'is-loading': areCoursesLoading})} style={{width: '100%'}}>
+                        <select name="cycle" placeholder="Cycle" style={{width: '100%'}}
+                                onChange={(e) => setSelectedCycle(e.target.value)}
+                                value={selectedCycle}
+                                disabled={areCoursesLoading}>
+                            <option value="" disabled hidden>Cycle</option>
+                            <option value={"prep"}>Prépa</option>
+                            <option value={"cii"}>CII</option>
+                            <option value={"ing"}>Ingé</option>
+                        </select>
+                    </div>
+                    <div style={{marginTop: 10}}/>
+                    <div className={classNames("select", {'is-loading': areCoursesLoading})} style={{width: '100%'}}>
+                        <Field component="select" name="courseId" placeholder="Matière" style={{width: '100%'}}
+                               disabled={areCoursesLoading || selectedCycle === ""}>
+                            <option value="" disabled hidden>Matière</option>
+                            {!areCoursesLoading && <CourseOptions courses={courses} selectedCycle={selectedCycle}/>}
                         </Field>
                     </div>
                     <ErrorMessage name="courseId" component="div"/>
+                    <div style={{marginTop: 10}}/>
+                    <Field type="text" name="tags" className="input"
+                           placeholder="Tags (chapitre, sujet...) séparés d'une virgule"/>
                     <div style={{marginTop: 10}}/>
                     <button type="submit" disabled={isSubmitting}
                             className={classNames("button", "is-info", {"is-loading": isSubmitting})}>
