600e05da3ffeaabc75be07bb6f30b6ca341075ac
NicolasRoyer <nicolas.royer@isep.fr>
2019-05-24 19:24:26 +0200

    upload avec save updaté

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,34 +1,52 @@
 package controllers;
 
-import play.libs.Files.TemporaryFile;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
-import play.data.Form;
 import play.data.FormFactory;
 import javax.inject.*;
-import java.io.File;
-import models.Sheet;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class HomeController extends Controller {
 
 	@Inject
 	FormFactory formFactory;
 	
-
-  public Result upload(Http.Request request) {
+	
+  public Result upload(Http.Request request) throws IOException {
 	  
 	
 	File file = request.body().asRaw().asFile();
     if (file != null) {
-      TemporaryFile File = file.getRef();
-      File.copyTo(Paths.get("/file/destination.jpg"), true);
+    Files.copy(file.toPath(), Paths.get("C:\\Users\\nroye\\Desktop\\Technoweb\\Destination.png"));
       return ok("File uploaded");
     } else {
       return badRequest().flashing("error", "Missing file");
     }
   }
 }
-  
+/*
+public void saveReport(File file)
+	    throws IOException
+	{
+	    final String content = report.getProperty("TEXT");
+	    final Path path = Paths.get("logKMAX.txt");
+
+	    try (
+	        final BufferedWriter writer = Files.newBufferedWriter(path,
+	            StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+	    ) {
+	        writer.write(content);
+	        writer.flush();
+	    }
+	}
+}
+  */
