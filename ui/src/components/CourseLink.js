791dacddf53dc026842d253d296b1d0807da18b0
Marin Godechot <marin.godechot@gmail.com>
2019-05-26 19:40:27 +0200

    Course navigation (#16)
    * show real courses on home page
    
    * use navlinks

--- ui/src/components/CourseLink.js
+++ ui/src/components/CourseLink.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import {NavLink} from "react-router-dom";
 
 function CourseLink(props) {
     const {to, text} = props;
     return (
-        <a href={to} className="button is-medium is-fullwidth" style={{marginBottom: "1.5rem"}}>{text}</a>
+        <NavLink to={to} className="button is-medium is-fullwidth" style={{marginBottom: "1.5rem"}}>{text}</NavLink>
     );
 
 }
